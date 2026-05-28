302108c96b461ec5ec742c8a4de8b94e5ae3c2fb
espinosaleonardo1234-eng <espinosaleonardo1234@gmail.com>
2026-05-28 03:01:26 +0000

    suma 5

---
+++ clase2.c/pro5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    //NEGACION
+    int x,y;
+    x=(!(7>15));
+    printf("%d",x);
+    y=(!0);
+    printf("%d",y);
+    //CONJUSION
+    x=(35>20)&&(20<=23);
+    
+    return 0;
+}
