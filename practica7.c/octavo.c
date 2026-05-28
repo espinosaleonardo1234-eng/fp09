5d3ed7748b0f3c2eb177c99277466747e70fdc0e
espinosaleonardo1234-eng <espinosaleonardo1234@gmail.com>
2026-05-28 04:13:22 +0000

    muchos codigos

---
+++ practica7.c/octavo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/*
+INSTRUCCIONES:
+A >= 90
+B >= 80
+C >= 70
+F < 70
+*/
+
+int main() {
+    int cal;
+    printf ("Ingresa la calificacion: \n");
+    scanf("%d", &cal);
+
+    if (cal >= 90) {
+        printf("A\n");
+    } else if (cal >= 80) {
+        printf("B\n");
+    } else if (cal >= 70) {
+        printf("C\n");
+    } else {
+        printf("F\n");
+    }
+
+    return 0;
+}
