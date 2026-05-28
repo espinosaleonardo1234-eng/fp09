27aa103ea0313dbd15b25c218cde43f98937a491
espinosaleonardo1234-eng <espinosaleonardo1234@gmail.com>
2026-05-28 04:14:24 +0000

    muchos codigos

---
+++ practica7.c/decimo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+INSTRUCCIONES:
+Completa el switch usando la variable "dia"
+*/
+
+int main() {
+    int dia;
+    printf("Ingresar un numero mayor a 1 \n");
+    scanf("%d", &dia);
+
+    switch (dia) {
+        case 1: printf("Lunes\n"); break;
+        case 2: printf("Martes\n"); break;
+        case 3: printf("Miercoles\n"); break;
+        case 4: printf("Jueves\n"); break;
+        case 5: printf("Viernes\n"); break;
+        case 6: printf("Sabado\n"); break;
+        case 7: printf("Domingo\n"); break;
+        default: printf("Dia invalido\n");
+    }
+
+    return 0;
+}
